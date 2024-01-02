bd4f5e40ea16d7f4c0e38f508560b4ef248a688c
ary <ary.obenholzner@gmail.com>
2024-01-03 00:36:03 +0100

    add influxdb and save data

--- updateEnergyPrices.go
+++ updateEnergyPrices.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/go-co-op/gocron/v2"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func startUpdateJob() {
@@ -18,7 +20,7 @@ func startUpdateJob() {
 
 	_, err = scheduler.NewJob(
 		gocron.OneTimeJob(gocron.OneTimeJobStartImmediately()),
-		gocron.NewTask(updateEnergyPrices),
+		gocron.NewTask(func() { updateEnergyPrices() }),
 	)
 	if err != nil {
 		log.Fatal("could not create job", err)
@@ -28,14 +30,21 @@ func startUpdateJob() {
 }
 
 func updateEnergyPrices() {
-	//todo reschedule retry
-	responseData, err := fetchEnergyPrices()
-	if err != nil {
-		log.Fatal("error while fetching API: ", err)
-		return
-	}
+	var err error
 
-	fmt.Println(responseData.Tariff)
+	retryCount := 60 * 24
+	for i := 0; i < retryCount; i++ {
+		responseData, err := fetchEnergyPrices()
+		if err == nil {
+			writeToDb(responseData)
+			return
+		}
+		time.Sleep(time.Minute)
+
+	}
+	if err != nil {
+		log.Println("Fetch request failed")
+	}
 }
 
 func fetchEnergyPrices() (*Response, error) {
@@ -55,4 +64,31 @@ func fetchEnergyPrices() (*Response, error) {
 	}
 
 	return &parsedResponse, nil
+}
+
+func writeToDb(priceData *Response) {
+	influxHost := os.Getenv("INFLUX_HOST")
+	token := os.Getenv("INFLUX_TOKEN")
+	bucket := os.Getenv("INFLUX_BUCKET")
+	org := os.Getenv("INFLUX_ORG")
+
+	influxClient := influxdb2.NewClient(influxHost, token)
+	writeApi := influxClient.WriteAPIBlocking(org, bucket)
+
+	for _, data := range priceData.Data {
+		point := influxdb2.NewPoint(
+			"energy-price",
+			map[string]string{"unit": priceData.Unit},
+			//todo add flat tax variable
+			map[string]interface{}{"value": data.Value + 1.44},
+			data.Date.Time,
+		)
+
+		err := writeApi.WritePoint(context.Background(), point)
+		if err != nil {
+			log.Println("error while writing to db: ", err)
+		}
+	}
+	influxClient.Close()
+	log.Println("Wrote %d points to db", len(priceData.Data))
 }
