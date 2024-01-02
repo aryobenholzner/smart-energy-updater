fc4070947d2f64a8d31a46578ab0f925d49f7387
aryobenholzner <ary.obenholzner@gmail.com>
2024-01-02 18:27:13 +0000

    create forever routine with a cron job

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-co-op/gocron/v2"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	scheduler, err := gocron.NewScheduler()
+	if err != nil {
+		fmt.Println("could not create scheduler", err)
+		return
+	}
+
+	_, err = scheduler.NewJob(
+		gocron.DurationJob(2*time.Second),
+		gocron.NewTask(func() {
+			fmt.Println("hello from the job")
+		}),
+	)
+	if err != nil {
+		fmt.Println("could not create job", err)
+		return
+	}
+
+	scheduler.Start()
+
+	go forever()
+
+	quitChannel := make(chan os.Signal, 1)
+	signal.Notify(quitChannel, syscall.SIGINT, syscall.SIGTERM)
+	<-quitChannel
+	//time for cleanup before exit
+	fmt.Println("Adios!")
+}
+
+func forever() {
+	for {
+		time.Sleep(time.Second)
+	}
 }
